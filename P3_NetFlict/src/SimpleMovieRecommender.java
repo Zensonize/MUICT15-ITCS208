031f184779bcf483c6c5b086320624a698fd7ff2
LEVEL51 <krittametk@hotmail.com>
2018-04-19 21:36:37 +0700

    Finished load file

--- P3_NetFlict/src/SimpleMovieRecommender.java
+++ P3_NetFlict/src/SimpleMovieRecommender.java
@@ -1,36 +1,122 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
 
 public class SimpleMovieRecommender implements BaseMovieRecommender {
 
+	Map<Integer, Movie> movies;
+	Map<Integer, User> users;
+	
 	@Override
 	public Map<Integer, Movie> loadMovies(String movieFilename) {
 		// TODO Auto-generated method stub
-		return null;
+		Map<Integer, Movie> movieStream = new HashMap<Integer, Movie>();
+		
+		try {
+			File moviesCSV = new File(movieFilename);
+			LineIterator it = FileUtils.lineIterator(moviesCSV, "UTF-8");
+			String regex = "(\\d+),(\"?)(.+) [(](\\d{4})[)](\"?),(.+)";
+			
+			 try {
+			   while (it.hasNext()) {
+			     String stream = it.nextLine();
+			     Pattern p = Pattern.compile(regex);
+				Matcher m = p.matcher(stream);
+				if(m.find()) {
+					int mid = Integer.parseInt(m.group(1));
+					String title = m.group(3);
+					int year = Integer.parseInt(m.group(4));
+					String[] tags = m.group(6).split("\\|");
+					
+					if(!movieStream.containsKey(mid))movieStream.put(mid,new Movie(mid,title,year));
+					
+					for(String k: tags) {
+						movieStream.get(mid).addTag(k);
+					}
+				}
+			   }
+			 } finally {
+			   LineIterator.closeQuietly(it);
+			 }
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return movieStream;
 	}
 
 	@Override
-	public Map<Integer, User> loadUsers(String ratingFilename) {
-		// TODO Auto-generated method stub
-		return null;
+	public Map<Integer, User> loadUsers(String userFilename) {
+		// TODO Auto-generated method stub 
+		Map<Integer, User> userStream = new HashMap<Integer, User>();
+		
+		try {
+			File usersCSV = new File(userFilename);
+			LineIterator it = FileUtils.lineIterator(usersCSV, "UTF-8");
+			
+			try {
+				while (it.hasNext()) {
+			     String[] stream = it.nextLine().split("\\,");
+			     if(stream.length == 4) {
+			    	 try {
+			    		 int uid = Integer.parseInt(stream[0]);
+				    	 int mid = Integer.parseInt(stream[1]);
+				    	 double rating = Double.parseDouble(stream[2]);
+				    	 long timestamp = Long.parseLong(stream[3]);
+				    	 
+				    	 if(userStream.containsKey(uid)) {										// If user exist 
+				    		 userStream.get(uid).addRating(movies.get(mid), rating, timestamp);
+						 }
+							
+						 else {
+							 userStream.put(uid, new User(uid));
+							 userStream.get(uid).addRating(movies.get(mid), rating, timestamp);
+						 }
+			    	 } catch(NumberFormatException e) {
+//			    		 e.printStackTrace();
+			    	 }
+			    	 
+			     }
+				
+			   }
+			 } finally {
+			   LineIterator.closeQuietly(it);
+			 }
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+				
+		return userStream;
 	}
 
 	@Override
 	public void loadData(String movieFilename, String userFilename) {
 		// TODO Auto-generated method stub
-
+		movies = loadMovies(movieFilename);
+		users = loadUsers(userFilename);
 	}
 
 	@Override
 	public Map<Integer, Movie> getAllMovies() {
 		// TODO Auto-generated method stub
-		return null;
+		return movies;
 	}
 
 	@Override
 	public Map<Integer, User> getAllUsers() {
 		// TODO Auto-generated method stub
-		return null;
+		return users;
 	}
 
 	@Override
